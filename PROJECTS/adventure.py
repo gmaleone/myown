7595fdfd46634227a614d066bc112ac90f35cbb4
gmaleone <itzmegema@gmail.com>
2026-08-28 10:59:32 -0700

    day2ofgame

--- PROJECTS/adventure.py
+++ PROJECTS/adventure.py
@@ -1,3 +1,14 @@
 print("=== THE FOREST ADVENTURE ===")
 print("You wake up in a dark forest, There are two paths ahead.")
 
+health = 100
+inventory = []
+
+playing = True
+while playing:
+    print("\nYour health:", health)
+    print("Your inventory:", inventory)
+
+    choice = input("\nDo u go LEFT or RIGHT ?").lower()
+    print("Your choice:", choice)
+    playing = False
